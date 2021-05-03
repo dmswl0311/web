7427699fd7443d88594d19014e8030ea51a4c5dd
dmswl0311 <dmswl_0311@naver.com>
2021-05-03 10:35:16 +0900

    고객센터- 공지사항 css,html 완성

---
+++ sub_footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+<!-- 하단 정보 -->   
+    <div>
+        <div class="f-logo">
+            <h1><a href="../index.html"><img src="../img/logo04.png" alt=""><span>SAMSUNG LIONS</br>삼성라이온즈야구단</span></a></h1>
+        </div>
+        <div class="f-copy">
+            <p>copyright(c) SAMSUNG LIONS. All Rights Reserved</p>
+        </div>
+    </div>
+</div>
---
+++ sub_header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="Generator" content="EditPlus®">
+    <meta name="Author" content="">
+    <meta name="Keywords" content="">
+    <meta name="Description" content="">
+    <!-- 반응형 웹 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=yes">
+
+    <title>NEW BLUE! NEW LIONS! - 삼성라이온즈</title>
+    <link rel="shortcut icon" href="../img/favicon.ico"/>
+    <script src="../js/jquery-1.12.4.min.js"></script>
+    <script src="../js/jquery-ui.min.js"></script>
+    <script src="../js/swiper.min.js"></script>
+    <link rel="stylesheet" href="../js/swiper.min.css" />
+
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- 폰트 -->
+    <link
+    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&display=swap"
+    rel="stylesheet"/>
+    <!-- 모바일 -->
+    <link rel="stylesheet" media="screen and (max-width: 480px)" href="../css/480.css">
+    <!-- 태블릿 세로--> 
+    <link rel="stylesheet" media="screen and (min-width: 481px) and (max-width: 720px)" href="../css/720.css">
+    <!-- 태블릿 가로-->
+    <link rel="stylesheet" media="screen and (min-width: 721px) and (max-width: 1024px)" href="../css/1024.css">
+    <!-- 데스크탑 -->
+    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="../css/pc.css">
+    <!-- Swiper JS -->
+    <script src="../js/swiper.min.js"></script>
+    <link rel="stylesheet" href="../css/swiper.min.css">
+    <!-- 애니메이션 -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <!-- 한 화면에 보여주는 컨텐츠를 꽉 차게끔 보여주는 -->
+    <link href="../js/fullpage.min.css" rel="stylesheet">
+    <script src="../js/fullpage.min.js"></script>
+    <!-- auto fix -->
+    <script src="../js/autofix.js"></script>
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+	<!-- 콘텐츠 시작 -->
+	<div class="header">
+	<!-- 로고,메뉴 -->
+		<div class="header-box">
+			<h1><a href="../index.html"><img src="../img/logo03.jpg" alt="동그란로고"></a></h1>
+			<div class="tnb">
+				<ul>
+					<li><a href="#none">로그인</a></li>
+					<li><a href="#none">회원가입</a></li>
+	<!-- 				<li><a href="#none">로그아웃</a></li> -->
+	<!-- 				<li><a href="#none">정보수정</a></li> -->
+				</ul>
+			</div>
+			<div class="gnb"> <!-- general navigator -->
+				<ul>
+					<li><a href="#none"><span>팀 소개</span></a></li>
+					<li><a href="../bbs/notice-list.html"><span>고객센터</span></a></li>
+					<li><a href="#none"><span>예매</span></a></li>
+					<li><a href="#none"><span>예매 확인</span></a></li>
+				</ul>
+			</div>
+			
+		</div>
+	</div>
+
+<script>
+		AOS.init();
+
+		$(document).ready(function(){
+		$(".header").autofix_anything();
+	});
+	</script>
+</body>
+</html>
